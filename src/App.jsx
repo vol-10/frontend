8e6a4b7095fe46c42ab2c0ebca393ee2e84bbb03
Daiki Io <nirasyuu@gmail.com>
2024-07-22 03:25:45 +0900

    途中経過

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
+import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import InputPicture from "./input.jsx"
+import Routess from './Routes.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const navigate = useNavigate();
+
+  const handleGoInputPicture = () => {
+    navigate('/input'); // ボタンをクリックしたときに /page2 へナビゲート
+  };
   return (
     <>
       <div>
+        hello worbv
+        
+
+      <h1>Input Picture Component</h1>
+      <Link to="/input">InputPicture</Link>
+      <button onClick={handleGoInputPicture}>Go to Page 2</button>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,8 +44,22 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <Routes>
+        <Route path="/input" component={InputPicture} />
+        <Route path="/" component={App} exact />
+      </Routes>
+
     </>
   )
 }
 
 export default App
+
+{/*
+  <Router>
+<Routes>
+<Route path="/" element={<InputPicture />} /> 
+</Routes>
+</Router>
+*/}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import InputPicture from './input.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <Router>
     <App />
+  </Router>,
+
   </React.StrictMode>,
 )
