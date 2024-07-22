c84b01ce1881a0ed2098e502f26c73472c38afcc
Daiki Io <nirasyuu@gmail.com>
2024-07-22 11:09:40 +0900

    ページ遷移を実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
+import { BrowserRouter , Route, Routes, Link} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import InputPicture from "./input.jsx"
 import Routess from './Routes.jsx'
-
+import Home from "./Home"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,38 +17,22 @@ function App() {
   const handleGoInputPicture = () => {
     navigate('/input'); // ボタンをクリックしたときに /page2 へナビゲート
   };
+
+  const handleGoHome = () => {
+    navigate('/'); // ボタンをクリックしたときに /page2 へナビゲート
+  };
   return (
     <>
       <div>
-        hello worbv
-        
-
-      <h1>Input Picture Component</h1>
       <Link to="/input">InputPicture</Link>
-      <button onClick={handleGoInputPicture}>Go to Page 2</button>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <button onClick={handleGoInputPicture}>Go to Input</button>
+      <button onClick={handleGoHome}>Go to Home</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
 
       <Routes>
-        <Route path="/input" component={InputPicture} />
-        <Route path="/" component={App} exact />
+      <Route path={"/"} element={<Home />}/>
+        <Route path="/input" element={InputPicture()} />
+       
       </Routes>
 
     </>
